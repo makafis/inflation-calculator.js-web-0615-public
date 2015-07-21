148aadd27a56cab8bc371b742742a5a5d67b50f5
makafis <vinnyalfieri@gmail.com>
2015-07-21 16:20:37 -0400

    IT WORKS but fails your spec

--- public/javascripts/custom/inflation-calculator.js
+++ public/javascripts/custom/inflation-calculator.js
@@ -1,1 +1,52 @@
 // code your functions here
+
+
+function InflationCalculator(start, end, amount){
+  this.start = start;
+  this.end = end;
+  this.amount = amount;
+
+
+};
+
+function addPriceToPage(price){
+  if (price.includes("$")){
+    var returnPrice = price.replace("$","");  
+  } else {
+    var returnPrice = price;
+  };
+  // $('#endPrice').val(returnPrice);
+  return returnPrice;
+};
+
+function fetchOptions(){
+  var start = $('#startDate').val();
+  var end = $('#endDate').val();
+  var amount = $('#startPrice').val();  
+  var returnValue = new InflationCalculator(start, end, amount);
+  return returnValue;
+};
+
+function fetchEndPrice(){
+
+
+};
+
+function priceFor(param1, param2){
+ var apiUrl = 'https://www.statbureau.org/calculate-inflation-price-jsonp?jsoncallback=?';
+  $.getJSON(apiUrl, {
+      country: 'united-states',
+      start: param1.start,
+      end: param1.end,
+      amount: param1.amount,
+      format: true
+  }).done(function (data) {
+
+    $('#endPrice').val(data);
+  });
+
+
+};
+
+
+// var result = inflationCalculator(userInput);
--- public/javascripts/custom/on-click.js
+++ public/javascripts/custom/on-click.js
@@ -3,5 +3,16 @@
 $(document).ready(function() {
 
   // add your on click event handler here
+
+  $("#calc").click(function(){
+
+    var x = fetchOptions();
+    x.amount = addPriceToPage(x.amount);
+    priceFor(x, x);
+    // debugger;
+  });
+
+
+
   
 });
